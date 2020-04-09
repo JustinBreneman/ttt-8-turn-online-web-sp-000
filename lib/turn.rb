c2355965f475f620c463e6208bba807d6bc46363
Justin Breneman <justin.breneman@yahoo.com>
2020-04-09 08:34:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,10 +18,11 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  until valid_move?(board, index)
+  if valid_move?(board, index) == false
     puts "Invalid move, please select a new one."
     turn(board)
   end
+  #puts "Invalid move, please select a new one."
   move(board, index)
   display_board(board)
 end
